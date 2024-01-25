561dc7d690bbc283420c507caf22b3c3665205ba
lucharles <54488953+lucharles@users.noreply.github.com>
2024-01-24 22:29:07 -0600

    Quick documentation updates

--- HOCMO_package/setup.py
+++ HOCMO_package/setup.py
@@ -1,14 +1,18 @@
 from setuptools import find_packages, setup
-
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 with open("requirements.txt") as handle:
     dependencies = list(handle.readlines())
 
 setup(
     
     name="hocmo",
-    version="1.0.0",
+    version="1.0.1",
     license='MIT',
     description = 'A Generalized Higher-Order Correlation Model (HOCMO) tool to generate scores modeling the strength of the relationship between triplicate entities using a tensor-based approach',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author = 'Charles Lu',
     author_email = 'lucharles@wustl.edu',
     install_requires = dependencies,
